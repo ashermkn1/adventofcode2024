008c6cdd607914e19a9a35436b0daadd5078ee6d
ashermkn1 <ashkornfeld@gmail.com>
2024-12-04 17:43:08 -0500

    day 4

--- lib/day3.ml
+++ lib/day3.ml
@@ -1,17 +1,51 @@
 open! Imports
+open Core
 
 module M = struct
   (* Type to parse the input into *)
-  type t = unit
+  type t = string
 
   (* Parse the input to type t, invoked for both parts *)
-  let parse _inputs = ()
+  let parse inputs = inputs
 
   (* Run part 1 with parsed inputs *)
-  let part1 _ = ()
+  let part1 memory =
+    let re = Re2.create_exn "mul\\((\\d+,\\d+)\\)" in
+    let matches = Re2.find_all_exn ~sub:(`Index 1) re memory in
+    let res =
+      List.fold matches ~init:0 ~f:(fun acc mul ->
+          let nums = String.split mul ~on:',' |> List.map ~f:Int.of_string in
+          match nums with
+          | [a; b] -> acc + (a * b)
+          | _ -> failwith "unreachable" )
+    in
+    printf "Part 1: %d\n" res ; ()
+
+  type instr = Do | Dont | Mul of int * int
 
   (* Run part 2 with parsed inputs *)
-  let part2 _ = ()
+  let part2 memory =
+    let re = Re2.create_exn "(do(n't)?\\(\\))|mul\\(\\d+,\\d+\\)" in
+    let cmds =
+      Re2.find_all_exn re memory
+      |> List.map ~f:(fun cmd ->
+             if String.is_prefix cmd ~prefix:"don't" then Dont
+             else if String.is_prefix cmd ~prefix:"do" then Do
+             else
+               let num_str = String.slice cmd 4 (String.length cmd - 1) in
+               let nums =
+                 String.split num_str ~on:',' |> List.map ~f:Int.of_string
+               in
+               Mul (List.hd_exn nums, List.hd_exn (List.tl_exn nums)) )
+    in
+    let res, _ =
+      List.fold cmds ~init:(0, true) ~f:(fun (acc, enabled) -> function
+        | Do -> (acc, true)
+        | Dont -> (acc, false)
+        | Mul (a, b) ->
+            if not enabled then (acc, enabled) else (acc + (a * b), enabled) )
+    in
+    printf "Part 2: %d\n" res ; ()
 end
 
 include M
--- lib/day4.ml
+++ lib/day4.ml
@@ -1,24 +1,105 @@
 open! Imports
+open Core
 
 module M = struct
   (* Type to parse the input into *)
-  type t = unit
+  type t = char array array
 
   (* Parse the input to type t, invoked for both parts *)
-  let parse _inputs = ()
+  let parse input =
+    let lines = String.split_lines input in
+    let rows = List.length lines in
+    let cols = String.length (List.hd_exn lines) in
+    let grid = Array.make_matrix ~dimx:rows ~dimy:cols '.' in
+    List.iteri lines ~f:(fun row line ->
+        List.iteri (String.to_list line) ~f:(fun col c ->
+            grid.(row).(col) <- c ) ) ;
+    grid
+
+  let in_bounds grid r c =
+    let rows = Array.length grid in
+    let cols = Array.length grid.(0) in
+    r >= 0 && c >= 0 && r < rows && c < cols
+
+  let next = function
+    | 'X' -> 'M'
+    | 'M' -> 'A'
+    | 'A' -> 'S'
+    | 'S' -> 'S'
+    | _ -> failwith "unreachable"
+
+  let dirs = List.cartesian_product [-1; 0; 1] [-1; 0; 1]
+
+  let search grid r c =
+    let along_dir (dr, dc) =
+      let good = ref true in
+      let curr = ref 'X' in
+      for i = 0 to 3 do
+        let r' = r + (i * dr) in
+        let c' = c + (i * dc) in
+        good :=
+          !good && in_bounds grid r' c' && Char.equal grid.(r').(c') !curr ;
+        curr := next !curr
+      done ;
+      !good
+    in
+    List.count dirs ~f:along_dir
 
   (* Run part 1 with parsed inputs *)
-  let part1 _ = ()
+  let part1 grid =
+    let res =
+      Array.foldi grid ~init:0 ~f:(fun r acc row ->
+          Array.foldi row ~init:acc ~f:(fun c acc _ ->
+              acc + search grid r c ) )
+    in
+    printf "Part 1: %d\n" res ; ()
+
+  let search2 grid r c =
+    let inbound = in_bounds grid in
+    let cross_in_bounds =
+      inbound r c
+      && inbound (r + 1) (c + 1)
+      && inbound (r - 1) (c - 1)
+      && inbound (r + 1) (c - 1)
+      && inbound (r - 1) (c - 1)
+    in
+    cross_in_bounds
+    && Char.equal grid.(r).(c) 'A'
+    && ( Char.equal grid.(r + 1).(c + 1) 'S'
+         && Char.equal grid.(r - 1).(c - 1) 'M'
+       || Char.equal grid.(r + 1).(c + 1) 'M'
+          && Char.equal grid.(r - 1).(c - 1) 'S' )
+    && ( Char.equal grid.(r + 1).(c - 1) 'S'
+         && Char.equal grid.(r - 1).(c + 1) 'M'
+       || Char.equal grid.(r + 1).(c - 1) 'M'
+          && Char.equal grid.(r - 1).(c + 1) 'S' )
 
   (* Run part 2 with parsed inputs *)
-  let part2 _ = ()
+  let part2 grid =
+    let res =
+      Array.foldi grid ~init:0 ~f:(fun r acc row ->
+          Array.foldi row ~init:acc ~f:(fun c acc _ ->
+              if search2 grid r c then succ acc else acc ) )
+    in
+    printf "Part 2: %d\n" res ; ()
 end
 
 include M
 include Day.Make (M)
 
 (* Example input *)
-let example = ""
+let example =
+  "MMMSXXMASM\n\
+   MSAMXMSMSA\n\
+   AMXSXMAAMM\n\
+   MSAMASMSMX\n\
+   XMASAMXAMM\n\
+   XXAMMXXAMA\n\
+   SMSMSASXSS\n\
+   SAXAMASAAA\n\
+   MAMMMXMMMM\n\
+   MXMXAXMASX"
 
 (* Expect test for example input *)
-let%expect_test _ = run example ; [%expect {| |}]
+let%expect_test _ = run example ; [%expect {| Part 1: 18
+Part 2: 9|}]
